a96b4019ed9619ec0a4546db57a6cac68ead045a
Diego O <de.ortiz10@uniandes.edu.co>
2017-05-14 21:25:16 -0500

    se intenta acceeder a la api

--- imports/api/methods.js
+++ imports/api/methods.js
@@ -3,10 +3,22 @@ import { check } from 'meteor/check';
 import  Wunderground from 'wunderground-api';
 
 //var Wunderground = require('wunderground-api');
-var client = new Wunderground('your api key here');
+//var client = new Wunderground('');
 
+Meteor.methods({
 
-client.conditions(opts, function(err, data) {
-    if (err) throw err;
-    else console.log(data);
+    'Wunderground.condition'(query){
+        var toret;
+    console.log("buscando lugar..");
+        let myWundergound= Wunderground('');
+        return myWundergound.conditions(query);
+    //  client.conditions(query,function(err, conditions) {
+    //     if (err) throw err;
+    //
+    //     else console.log(conditions.results);
+    //     toret=conditions.results;
+    // });
+    //  return toret;
+    }
+
 });
--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,8 +1,54 @@
 import React, { Component } from 'react';
-
-import Task from './Task.jsx';
+import ReactDOM from 'react-dom';
+import { Meteor } from 'meteor/meteor';
+import { Template } from 'meteor/templating';
+//import Task from './Task.jsx';
 
 // App component - represents the whole app
-export default class App extends Component {
+ class App extends Component {
 
-}
+
+     render() {
+         return (
+             <div className="container">
+                 <header>
+                     <h1>Todo List</h1>
+                 </header>
+                 <form className="climate-form" onSubmit={this.Climate.bind(this)} >
+
+                    <label> Ciudad</label>
+                     <input name="city" className="city" type="text" ref="city" label="Ciudad"/>
+                     <label> Pais</label>
+                     <input name="country" className="country" type="text" ref="country" label="Pais"/>
+                     <button type="submit" >Get conditions </button>
+                 </form>
+
+
+
+                 <br/>
+
+             </div>
+         );
+     }
+
+     Climate(event) {
+         event.preventDefault();
+         const text1 = ReactDOM.findDOMNode(this.refs.city).value.trim();
+        const text2 = ReactDOM.findDOMNode(this.refs.country).value.trim();
+         let arr= {
+             city: text1,
+             state: text2
+         };
+         Meteor.call("Wunderground.condition", arr, (err, res)=> {
+             if (err)
+             {
+                 console.log(err);
+             }
+             console.log("made it!");
+             console.log(res.results);
+
+         })
+
+     }
+ }
+export default App;
