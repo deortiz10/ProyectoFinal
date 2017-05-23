3dea0d736bd1e153d55d77e2789e3a1e252ee643
klosorio10 <kl.osorio10@uniandes.edu.co>
2017-05-22 21:17:31 -0500

    Implementacion API weather con Colombia

--- imports/api/methods.js
+++ imports/api/methods.js
@@ -1,24 +1,30 @@
 import { Meteor } from 'meteor/meteor';
 import { check } from 'meteor/check';
 import  Wunderground from 'wunderground-api';
+import Accuweather from 'node-accuweather';
 
 //var Wunderground = require('wunderground-api');
 //var client = new Wunderground('');
 
 Meteor.methods({
 
-    'Wunderground.condition'(query){
-        var toret;
-    console.log("buscando lugar..");
-        let myWundergound= Wunderground('');
-        return myWundergound.conditions(query);
-    //  client.conditions(query,function(err, conditions) {
-    //     if (err) throw err;
-    //
-    //     else console.log(conditions.results);
-    //     toret=conditions.results;
-    // });
-    //  return toret;
+    'accuweather.conditions'(query){
+        console.log("buscando lugar..");
+        var code;
+        let myAccuweather = Accuweather()('FPFMtLTTFaQfO2Z2CWK5QrGh1xRgMQ7y');
+        myAccuweather.queryLocations(query).then(function(result) {
+          for(var i = 0; i<result.length; i++){
+            if(result[i].Country.LocalizedName == 'Colombia'){
+              code = result[i].Key;
+              myAccuweather.getCurrentConditions(code, {unit: "Celsius"}) // can be type string or Number
+                        .then(function(ciudad) {
+                            console.log(ciudad);
+                            return ciudad;
+              });
+            }
+          }
+        });
+
     }
 
 });
--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
-//import Task from './Task.jsx';
 
 // App component - represents the whole app
  class App extends Component {
@@ -15,11 +14,13 @@ import { Template } from 'meteor/templating';
                      <h1>Todo List</h1>
                  </header>
                  <form className="climate-form" onSubmit={this.Climate.bind(this)} >
-
+                 <div class="container">
                     <label> Ciudad</label>
                      <input name="city" className="city" type="text" ref="city" label="Ciudad"/>
-                     <label> Pais</label>
-                     <input name="country" className="country" type="text" ref="country" label="Pais"/>
+                     <div class="alert alert-warning">
+                     <strong>Warning!</strong> This alert box could indicate a warning that might need attention.
+                     </div>
+                </div>
                      <button type="submit" >Get conditions </button>
                  </form>
 
@@ -34,12 +35,8 @@ import { Template } from 'meteor/templating';
      Climate(event) {
          event.preventDefault();
          const text1 = ReactDOM.findDOMNode(this.refs.city).value.trim();
-        const text2 = ReactDOM.findDOMNode(this.refs.country).value.trim();
-         let arr= {
-             city: text1,
-             state: text2
-         };
-         Meteor.call("Wunderground.condition", arr, (err, res)=> {
+         query = text1
+         Meteor.call("accuweather.conditions", query, (err, res)=> {
              if (err)
              {
                  console.log(err);
