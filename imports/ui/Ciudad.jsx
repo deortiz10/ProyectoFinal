1625152940de3529ceccb99ec15622e0f0cc0a77
klosorio10 <kl.osorio10@uniandes.edu.co>
2017-05-25 19:20:51 -0500

    ciudad

---
+++ imports/ui/Ciudad.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { Meteor } from 'meteor/meteor';
+import { Template } from 'meteor/templating';
+
+// App component - represents the whole app
+ class Ciudad extends Component {
+
+  constructor(props){
+  super(props);
+
+  }
+
+  render(){
+  return(
+
+  );}
+
+}
+export default Ciudad;
